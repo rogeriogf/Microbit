ae71c6387f1bb8b827d3c839c08166f245d97244
rogeriogf <65512814+rogeriogf@users.noreply.github.com>
2023-09-29 14:35:04 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,55 @@
+input.onButtonPressed(Button.A, function () {
+    record.startRecording(record.BlockingState.Blocking)
+})
+input.onButtonPressed(Button.B, function () {
+    record.playAudio(record.BlockingState.Blocking)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+})
+music.play(music.stringPlayable("C D E F - F F - ", 300), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("C D C D - D D - ", 300), music.PlaybackMode.UntilDone)
+for (let index = 0; index < 10; index++) {
+	
+}
+basic.showLeds(`
+    . # # # .
+    . # . . .
+    . # # # .
+    . # . . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # . . .
+    . # . . .
+    . # # # .
+    `)
+basic.showLeds(`
+    . # # # .
+    # # . # .
+    # # # # .
+    # # # . .
+    # # # . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    # # . # #
+    # # . # #
+    # . # . #
+    # . . . #
+    `)
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . # . # .
-        # . # . .
-        # . . . .
-        `)
-    basic.pause(300)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . # . #
-        # # . # .
-        # . . . .
-        `)
-    basic.pause(300)
-    basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # #
-        . # # . #
-        # . . . .
-        `)
-    basic.pause(100)
+	
 })
